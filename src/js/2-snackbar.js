d581bcd85a39053029173bf2291f23e62b9d432e
SvitlanaLucky <svitlanalucky04@gmail.com>
2025-02-28 20:25:41 +0200

    snackbar

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,40 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const refs = {
+  form: document.querySelector('.form'),
+};
+
+refs.form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const delay = Number(refs.form.delay.value);
+  const state = refs.form.state.value;
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  promise
+    .then(delay => {
+      iziToast.success({
+        title: 'OK',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    });
+  refs.form.reset();
+});
